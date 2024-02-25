18d63221ac42c610624a7974143280525b672367
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2024-02-25 14:10:04 -0800

    Update pcScripts.js

--- painter-creative/pcScripts.js
+++ painter-creative/pcScripts.js
@@ -25,7 +25,7 @@ const BB = document.getElementById("bottomBar");
 	const imgLink = '"https://gcparksandresorts.github.io/main/painter-creative","_self"';
 
 	function createBottomBar(){
-		BB.innerHTML = "<img src='<img src='https://gcparksandresorts.github.io/main/webresources/images/painterCreative.png'>' onclick='window.open("+imgLink+")'>";
+		BB.innerHTML = "<img src='<img src='https://gcparksandresorts.github.io/main/webresources/images/painterCreative.png' onclick='window.open("+imgLink+")'>";
 		BB.innerHTML += "<a href='https://gcparksandresorts.github.io/main/careers/painter-creative'>Careers</a> \
 				<a href='https://gcparksandresorts.github.io/main/painter-creative/portfolio'>Portfolio</a> \
 				<a href='https://gcparksandresorts.github.io/main/painter-creative#contact'>Contact</a>";
